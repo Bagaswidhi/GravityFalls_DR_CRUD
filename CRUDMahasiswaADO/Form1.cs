d2eee9fa978a1a3637e38c0c63c2bbea27923a69
Bagaswidhi <Bagaswidhi10@gmail.com>
2026-04-10 11:22:23 +0700

    Menambahkan fungsi dari button load data

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -11,7 +11,7 @@ using System.Data.SqlClient;
 
 namespace CRUDMahasiswaADO
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         private readonly SqlConnection conn;
         private readonly string connectionString = "Data Source=MSI\\BAGASWIDHI;Initial Catalog=DBAkademikADO;Integrated Security=True";
@@ -38,6 +38,48 @@ namespace CRUDMahasiswaADO
         private void btnConnect_Click(object sender, EventArgs e)
         {
             ConnectDatabase();
+        }
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+
+                dataGridView1.Columns.Add("NIM", "NIM");
+                dataGridView1.Columns.Add("Nama", "Nama");
+                dataGridView1.Columns.Add("JenisKelamin", "Jenis Kelamin");
+                dataGridView1.Columns.Add("TanggalLahir", "Tanggal Lahir");
+                dataGridView1.Columns.Add("Alamat", "Alamat");
+                dataGridView1.Columns.Add("KodeProdi", "Kode Prodi");
+
+                string query = "SELECT * FROM Mahasiswa";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(
+                        reader["NIM"].ToString(),
+                        reader["Nama"].ToString(),
+                        reader["JenisKelamin"].ToString(),
+                        Convert.ToDateTime(reader["TanggalLahir"]).ToShortDateString(),
+                        reader["Alamat"].ToString(),
+                        reader["KodeProdi"].ToString()
+                    );
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
     }
 }
